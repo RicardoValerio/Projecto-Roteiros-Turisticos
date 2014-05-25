0a1297b98c6f5c9738588a8cf5080c428c8fcf60
Pedro Dantas <pedro.cdantas@gmail.com>
2014-05-25 19:52:54 +0100

    alterei o live search para receber tb um valor de GET

--- js/search.js
+++ js/search.js
@@ -1,46 +1,52 @@
 /*****************************************************************************
-*				LIVE-SEARCH BOX
-*****************************************************************************/
+ *				LIVE-SEARCH BOX
+ *****************************************************************************/
 
 /* JS File */
 
 // Start Ready
 $(document).ready(function() {
-
-	// Live Search
-	// On Search Submit and Get Results
-	function search() {
-		var query_value = $('input#search').val();
-		$('b#search-string').html(query_value);
-		if(query_value !== ''){
-			$.ajax({
-				type: "POST",
-				url: "procura.php",
-				data: { query: query_value },
-				cache: false,
-				success: function(html){
-					$("ul#results").html(html);
-				}
-			});
-		}return false;
-	}
-
-	$("input#search").on("keyup", function(e) {
-		// Set Timeout
-		clearTimeout($.data(this, 'timer'));
-
-		// Set Search String
-		var search_string = $(this).val();
-
-		// Do Search
-		if (search_string == '') {
-			$("ul#results").fadeOut();
-			$('h4#results-text').fadeOut();
-		}else{
-			$("ul#results").fadeIn();
-			$('h4#results-text').fadeIn();
-			$(this).data('timer', setTimeout(search, 100));
-		};
-	});
+    prepareSearch();
+    
+    $("input#search").on("keyup", function(e) {
+        prepareSearch();
+    });
 
 });
+
+function prepareSearch() {
+// Set Timeout
+    clearTimeout($.data("input#search", 'timer'));
+
+    // Set Search String
+    var search_string = $("input#search").val();
+
+    // Do Search
+    if (search_string == '') {
+        $("ul#results").fadeOut();
+        $('h4#results-text').fadeOut();
+    } else {
+        $("ul#results").fadeIn();
+        $('h4#results-text').fadeIn();
+        $("input#search").data('timer', setTimeout(search, 100));
+    }
+}
+
+// Live Search
+// On Search Submit and Get Results
+function search() {
+    var query_value = $('input#search').val();
+    $('b#search-string').html(query_value);
+    if (query_value !== '') {
+        $.ajax({
+            type: "POST",
+            url: "procura.php",
+            data: {query: query_value},
+            cache: false,
+            success: function(response) {
+                $("ul#results").html(response);
+            }
+        });
+    }
+    return false;
+}
