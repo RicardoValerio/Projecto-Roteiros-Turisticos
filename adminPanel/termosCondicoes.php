0ae59d6c1fbebdc7049d8d4c63e2dce1caaad90f
Pedro Dantas <pedro.cdantas@gmail.com>
2014-05-27 12:01:50 +0100

    speel check

--- adminPanel/termosCondicoes.php
+++ adminPanel/termosCondicoes.php
@@ -1,49 +1,46 @@
 <h1 style="font-size: 3em;
-text-align: center;
-margin-top: 75px;
-">Editar Termos & Condições</h1>
+    text-align: center;
+    margin-top: 75px;
+    ">Editar Termos & Condições</h1>
 
 <div style="margin-left: 69px;" id="editarRoteiro">
 
     <?php
-
-
     $sql = "SELECT * FROM texto";
 
     $result_termos = mysql_query($sql);
 
-    ?>
+    if (@mysql_num_rows($result_termos)) {
+        $row_termos = mysql_fetch_assoc($result_termos);
+        ?>
 
-    <?php if (@mysql_num_rows($result_termos)): ?>
-
-
-    <?php $row_termos = mysql_fetch_assoc($result_termos); ?>
-
-    <form id="formEditarTermos" action="processaEditarTermosCondicoes.php" method="post">
+        <form id="formEditarTermos" action="processaEditarTermosCondicoes.php" method="post">
 
 
-        <input type="hidden" name="i" value="<?php echo $row_termos['id']; ?>">
+            <input type="hidden" name="i" value="<?php echo $row_termos['id']; ?>">
 
-        <p>
-            <label for="termos">Termos e Condições:</label>
-            <textarea style="margin: 2px; height: 157px; width: 843px; resize: none;" name="termos" id="termos" placeholder="Insira uma breve descrição sobre o roteiro...">
-                <?php echo $row_termos['texto']; ?>
-            </textarea>
-        </p>
+            <p>
+                <label for="termos">Termos e Condições:</label>
+                <textarea style="margin: 2px; height: 157px; width: 843px; resize: none;" name="termos" id="termos" placeholder="Insira uma breve descrição sobre o roteiro...">
+                    <?php echo $row_termos['texto']; ?>
+                </textarea>
+            </p>
 
-        <input class="mySubmitButton" type="submit" value="Atualizar Termos e Condições" />
+            <input class="mySubmitButton" type="submit" value="Atualizar Termos e Condições" />
 
-    </form>
+        </form>
 
     </script>
 
     <script>
-    CKEDITOR.replace('termos');
+        CKEDITOR.replace('termos');
     </script>
 
-</div>
+    </div>
 
 
-<?php else: ?>
-    <?php echo "Não existe nada nessta tabela!"; ?>
-<?php endif ?>
+<?php
+} else {
+    echo "Não existe nada nesta tabela!";
+}
+?>
